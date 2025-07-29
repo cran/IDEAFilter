52528b2e40212bd38bc1001aa0bd8c560051e77d
Aaron Clark <clark.aaronchris@gmail.com>
2025-07-29 18:40:02 +0000

    version 0.2.1

--- inst/doc/IDEAFilter.R
+++ inst/doc/IDEAFilter.R
@@ -5,32 +5,32 @@ knitr::opts_chunk$set(
 )
 
 ## ----eval=FALSE---------------------------------------------------------------
-#  library(shiny)
-#  library(IDEAFilter)
-#  library(dplyr)
-#  shinyApp(
-#    ui = fluidPage(
-#      titlePanel("Filter Data Example"),
-#      fluidRow(
-#        column(8, dataTableOutput("data_summary")),
-#        column(4, IDEAFilter_ui("data_filter")))),
-#    server = function(input, output, session) {
-#      filtered_data <- IDEAFilter("data_filter", data = iris, verbose = FALSE)
-#      output$data_summary <-
-#        renderDataTable(filtered_data(),
-#      options = list(scrollX = TRUE, pageLength = 5))
-#    }
-#  )
+# library(shiny)
+# library(IDEAFilter)
+# library(dplyr)
+# shinyApp(
+#   ui = fluidPage(
+#     titlePanel("Filter Data Example"),
+#     fluidRow(
+#       column(8, dataTableOutput("data_summary")),
+#       column(4, IDEAFilter_ui("data_filter")))),
+#   server = function(input, output, session) {
+#     filtered_data <- IDEAFilter("data_filter", data = iris, verbose = FALSE)
+#     output$data_summary <-
+#       renderDataTable(filtered_data(),
+#     options = list(scrollX = TRUE, pageLength = 5))
+#   }
+# )
 
 ## ----eval=FALSE---------------------------------------------------------------
-#  library(shiny)
-#  library(IDEAFilter)
-#  app <- system.file("examples", "starwars_app", package = "IDEAFilter")
-#  runApp(app)
+# library(shiny)
+# library(IDEAFilter)
+# app <- system.file("examples", "starwars_app", package = "IDEAFilter")
+# runApp(app)
 
 ## ----eval=FALSE---------------------------------------------------------------
-#  list(
-#    is_droid = list(filter_na = TRUE, filter_fn = ~ isTRUE(.x)),
-#    mass = list(filter_fn = ~ .x < 50))
-#  )
+# list(
+#   is_droid = list(filter_na = TRUE, filter_fn = ~ isTRUE(.x)),
+#   mass = list(filter_fn = ~ .x < 50))
+# )
 
